465a9a740cf2dbc9219e8e731536890af37b3524
yisheng.Lv <lv.yisheng@hand-china.com>
2019-09-26 20:27:08 +0800

    持续集成测试

---
+++ src/main/java/org/hand/sayhellojenkins/SayhellojenkinsApplication.java
@@ -0,0 +1,13 @@
+package org.hand.sayhellojenkins;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SayhellojenkinsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SayhellojenkinsApplication.class, args);
+    }
+
+}
